37bd06c8a12ebb54270bde56d537b7bf9d340cc9
aQua <aQuaYi@users.noreply.github.com>
2017-05-15 11:32:14 +0800

    修改了适配器模式，更简洁。

--- designPattern/Adapter/main.go
+++ designPattern/Adapter/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"DesignModel/Adapter/adapter"
+	"fmt"
 )
 
 func main() {
-	pb := adapter.NewPrintBanner("Hello")
-	printWeakAndStrong(pb)
+	pb := newBanner("Hello")
+	pb.printWeak()
 }
 
-type print2 interface {
-	PrintWeak()
-	PrintStrong()
+/*
+以下是新的API
+banner.PrintWeak()与oldBanner.showWithBrackets()实现的一模一样的功能，但是，必须使用PrintWeak()的名字。
+*/
+
+type banner struct {
+	*oldBanner
 }
 
-func printWeakAndStrong(p print2) {
-	p.PrintWeak()
-	p.PrintStrong()
+func newBanner(str string) *banner {
+	return &banner{&oldBanner{str: str}}
+}
+
+func (b *banner) printWeak() {
+	//关键结构
+	//调用printWeak的时候，转向了showWithBrackets，实现了适配
+	b.showWithBrackets()
+}
+
+/*
+以下是旧的API
+oldBanner.ShowWithBrackets()输出banner的内容，并在前后加上括号。
+*/
+
+type oldBanner struct {
+	str string
+}
+
+func (ob *oldBanner) showWithBrackets() {
+	fmt.Println("(" + ob.str + ")")
 }
