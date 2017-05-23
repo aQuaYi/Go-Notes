c7a0549ad0bfbe97ff06a88f737bfad38b2d6a2d
aQua <aQuaYi@users.noreply.github.com>
2017-05-23 09:30:13 +0800

    添加了《抽象过程与具体步骤分离的优雅方式》的具体代码，但是总感觉还有改进的空间。

---
+++ Golang/interfaceFunc/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Handler interface {
+	Do(k, v interface{})
+}
+
+type HandlerFunc func(k, v interface{})
+
+func (f HandlerFunc) Do(k, v interface{}) {
+	f(k, v)
+}
+
+func Each(m map[interface{}]interface{}, h Handler) {
+	for k, v := range m {
+		h.Do(k, v)
+	}
+}
+
+func EachFunc(m map[interface{}]interface{}, f func(k, v interface{})) {
+	Each(m, HandlerFunc(f))
+}
+
+func selfAge(k, v interface{}) {
+	fmt.Printf("大家好，我叫%s, 今年%d岁\n", k, v)
+}
+
+func selfNumber(k, v interface{}) {
+	fmt.Printf("大家好，我叫%s, 学号是%d\n", k, v)
+}
+
+func main() {
+	persons := make(map[interface{}]interface{})
+	persons["张三"] = 20
+	persons["李四"] = 23
+	persons["王五"] = 26
+
+	EachFunc(persons, selfAge)
+	EachFunc(persons, selfNumber)
+}
