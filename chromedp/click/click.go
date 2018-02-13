1a2fd51ecf399896f7ee7bd128af0121cdf07b7f
aQua <aQua@aQuaYi.com>
2018-02-13 19:32:52 +0800

    学会了获取 LeetCode 中题目的函数值

---
+++ chromedp/click/click.go
@@ -0,0 +1,52 @@
+// Command click is a chromedp example demonstrating how to use a selector to
+// click on an element.
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	var err error
+
+	// create context
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// create chrome instance
+	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run task list
+	err = c.Run(ctxt, click())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// shutdown chrome
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// wait for chrome to finish
+	err = c.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func click() chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(`https://golang.org/pkg/time/`),
+		chromedp.WaitVisible(`#footer`),
+		chromedp.Click(`#pkg-overview`, chromedp.NodeVisible),
+		chromedp.Sleep(1500 * time.Second),
+	}
+}
