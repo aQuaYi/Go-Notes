30638a4a2c4cb664784ed0fe7d4e08c460d1d2ac
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-11 11:10:09 +0800

    添加了 gostub 环境变量的代码

--- TestLib/gostub/example/gostub_env_test.go
+++ TestLib/gostub/example/gostub_env_test.go
@@ -13,22 +13,28 @@ func TestStubSettedEnv(t *testing.T) {
 
 	Convey("设置环境变量 SSE", t, func() {
 		os.Setenv("SSE", "SSE")
+
 		Convey("如果，没有对其打桩的话", func() {
+
 			Convey("其中的变量值，应该是原先的值", func() {
 				So(os.Getenv("SSE"), ShouldEqual, "SSE")
 			})
 		})
+
 		Convey("如果，对其打桩的话", func() {
+			stubs.SetEnv("SSE", "SSE Stubed")
+			defer stubs.Reset()
+
 			Convey("其中的变量值，应该是 Stub 的值", func() {
-				stubs.SetEnv("SSE", "SSE Stubed")
-				defer stubs.Reset()
 				So(os.Getenv("SSE"), ShouldEqual, "SSE Stubed")
 			})
 		})
+
 		Convey("如果，打桩成 unset 的话", func() {
+			stubs.UnsetEnv("SSE")
+			defer stubs.Reset()
+
 			Convey("应该查询不到这个变量的值", func() {
-				stubs.UnsetEnv("SSE")
-				defer stubs.Reset()
 				_, ok := os.LookupEnv("SSE")
 				So(ok, ShouldBeFalse)
 			})
@@ -41,23 +47,25 @@ func TestStubUnsettedEnv(t *testing.T) {
 
 	Convey("unset 环境变量 USE", t, func() {
 		os.Unsetenv("USE")
+
 		Convey("如果，没有对其打桩的话", func() {
-			Convey("其中的变量值，应该是原先的值", func() {
-				So(os.Getenv("SSE"), ShouldEqual, "SSE")
-			})
-		})
-		Convey("如果，对其打桩的话", func() {
-			Convey("其中的变量值，应该是 Stub 的值", func() {
-				stubs.SetEnv("SSE", "SSE Stubed")
-				defer stubs.Reset()
-				So(os.Getenv("SSE"), ShouldEqual, "SSE Stubed")
-			})
-		})
-		Convey("如果，打桩成 unset 的话", func() {
+
 			Convey("应该查询不到这个变量的值", func() {
-				stubs.UnsetEnv("SSE")
-				defer stubs.Reset()
-				_, ok := os.LookupEnv("SSE")
+				_, ok := os.LookupEnv("USE")
+				So(ok, ShouldBeFalse)
+			})
+		})
+
+		Convey("如果，对其打桩的话", func() {
+			stubs.SetEnv("USE", "USE Stubed")
+
+			Convey("其中的变量值，应该是 Stub 的值", func() {
+				So(os.Getenv("USE"), ShouldEqual, "USE Stubed")
+			})
+			stubs.Reset()
+
+			Convey("Stubs.Reset() 后，应该依然查询不到该值", func() {
+				_, ok := os.LookupEnv("USE")
 				So(ok, ShouldBeFalse)
 			})
 		})
