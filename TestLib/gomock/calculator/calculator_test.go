4788fb7e8210421d90a3d6ef51778e8f53844fe2
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-13 13:27:49 +0800

    gomock 添加了 Do 的示例

--- TestLib/gomock/calculator/calculator_test.go
+++ TestLib/gomock/calculator/calculator_test.go
@@ -8,18 +8,17 @@ import (
 )
 
 func Test_Calculator_Add(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
 
 	Convey("生成新的 Calculator", t, func() {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
 		mockMath := NewMockMath(ctrl)
 		c := &Calculator{
 			math: mockMath,
 		}
 
 		Convey("如果， math 运行正常", func() {
-			mockMath.
-				EXPECT().
+			mockMath.EXPECT().
 				Add(1, 1).
 				Return(2)
 			Convey("那么， Calculator 显示正确的结果", func() {
@@ -28,14 +27,35 @@ func Test_Calculator_Add(t *testing.T) {
 		})
 
 		Convey("如果， math 运行 **不** 正常", func() {
-			mockMath.
-				EXPECT().
+			mockMath.EXPECT().
 				Add(1, 1).
+				Do(func(x, y int) {
+					Printf("Add(%d,%d)", x, y)
+				}).
 				Return(3)
 			Convey("那么， Calculator 出现错误的结果", func() {
 				So(c.Add(1, 1), ShouldNotEqual, 2)
 			})
 		})
+
+		Convey("如果你愿意，还可以给 Mock 对象处理代码", func() {
+			mockMath.EXPECT().
+				Add(gomock.Any(), gomock.Any()).
+				DoAndReturn(func(x, y int) int {
+					Printf("Add(%d,%d)", x, y)
+					if x > 65535 || y > 65535 {
+						panic("number is too big")
+					}
+					return x + y
+				}).
+				AnyTimes()
+			Convey("那么， Calculator 显示正确的结果", func() {
+				So(c.Add(1, 1), ShouldEqual, 2)
+				So(func() { c.Add(1, 65536) }, ShouldPanicWith, "number is too big")
+				So(func() { c.Add(65536, 1) }, ShouldPanicWith, "number is too big")
+			})
+		})
+
 	})
 
 }
