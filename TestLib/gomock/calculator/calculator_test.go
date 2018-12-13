06d6b15bf849fc265c469f24bbc5c434c20453e6
aQua <6028869+aQuaYi@users.noreply.github.com>
2018-12-13 10:57:19 +0800

    gomock 美化了样例的格式

--- TestLib/gomock/calculator/calculator_test.go
+++ TestLib/gomock/calculator/calculator_test.go
@@ -18,14 +18,20 @@ func Test_Calculator_Add(t *testing.T) {
 		}
 
 		Convey("如果， math 运行正常", func() {
-			mockMath.EXPECT().Add(1, 1).Return(2)
+			mockMath.
+				EXPECT().
+				Add(1, 1).
+				Return(2)
 			Convey("那么， Calculator 显示正确的结果", func() {
 				So(c.Add(1, 1), ShouldEqual, 2)
 			})
 		})
 
 		Convey("如果， math 运行 **不** 正常", func() {
-			mockMath.EXPECT().Add(1, 1).Return(3)
+			mockMath.
+				EXPECT().
+				Add(1, 1).
+				Return(3)
 			Convey("那么， Calculator 出现错误的结果", func() {
 				So(c.Add(1, 1), ShouldNotEqual, 2)
 			})
