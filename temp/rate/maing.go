ccaf9c727ccd3a2ae28dca607d940cc90bf21166
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-10 15:27:37 +0800

    小修改

--- temp/rate/maing.go
+++ temp/rate/maing.go
@@ -14,17 +14,16 @@ func main() {
 	b := a
 	ratio := 0.03
 	for i := 0; i < 500; i++ {
-		olda, oldb := a, b
+		oldA, oldB := a, b
 		r := rand.Intn(10) - 4
 		p := float64(100+r) / 100
 
 		if r <= 0 {
-			a, b = olda*p, oldb*p
+			a, b = oldA*p, oldB*p
 		} else {
-			ya, yb := olda*p-olda, oldb*p-oldb
+			ya, yb := oldA*p-oldA, oldB*p-oldB
 			fee := yb * ratio
-			a, b = olda+ya+fee, oldb+yb-fee
-			a += b
+			a, b = oldA+ya+fee, oldB+yb-fee
 		}
 
 		fmt.Printf("%10.2f, %10.2f, %10.5f, \n", a, b, a/(a+b))
