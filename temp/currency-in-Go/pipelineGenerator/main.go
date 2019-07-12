6e119b6251be5b32dc1a1f1a0e5682ed30abfaa6
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-12 14:21:29 +0800

    pipeline done

---
+++ temp/currency-in-Go/pipelineGenerator/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	done := make(chan interface{})
+	defer close(done)
+	for num := range take(done, repeat(done, 1), 10) {
+		fmt.Printf("%v ", num)
+	}
+	fmt.Println()
+
+	randInt := func() interface{} {
+		return rand.Int()
+	}
+	for num := range take(done, repeatFn(done, randInt), 7) {
+		fmt.Printf("%v \n", num)
+	}
+	fmt.Println()
+
+	for s := range toString(done, take(done, repeat(done, "L", "O", "V", "E"), 10)) {
+		fmt.Printf("%v ", s)
+	}
+	fmt.Println()
+
+}
+
+var repeat = func(
+	done <-chan interface{},
+	values ...interface{},
+) <-chan interface{} {
+	valueStream := make(chan interface{})
+	go func() {
+		defer close(valueStream)
+		for {
+			for _, v := range values {
+				select {
+				case <-done:
+					return
+				case valueStream <- v:
+				}
+			}
+		}
+	}()
+	return valueStream
+}
+
+var take = func(
+	done <-chan interface{},
+	valueStream <-chan interface{},
+	num int,
+) <-chan interface{} {
+	takeStream := make(chan interface{})
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ {
+			select {
+			case <-done:
+				return
+			case takeStream <- <-valueStream:
+			}
+		}
+	}()
+	return takeStream
+}
+
+var repeatFn = func(
+	done <-chan interface{},
+	fn func() interface{},
+) <-chan interface{} {
+	valueStream := make(chan interface{})
+	go func() {
+		defer close(valueStream)
+		for {
+			select {
+			case <-done:
+				return
+			case valueStream <- fn():
+			}
+		}
+	}()
+	return valueStream
+}
+
+var toString = func(
+	done <-chan interface{},
+	valueStream <-chan interface{},
+) <-chan string {
+	stringStream := make(chan string)
+	go func() {
+		defer close(stringStream)
+		for v := range valueStream {
+			select {
+			case <-done:
+				return
+			case stringStream <- v.(string):
+			}
+		}
+	}()
+	return stringStream
+}
