dd6fc6af58b96274368bef87ef4fc1caea84c993
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-07-16 16:44:38 +0800

    heartbeat done

---
+++ temp/currency-in-Go/heartBeat/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+func main() {
+	done := make(chan interface{})
+	time.AfterFunc(10*time.Second, func() {
+		log.Println("close done")
+		close(done)
+	})
+
+	const timeout = 2 * time.Second
+	heartbeat, results := doWork(done, timeout/2)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if !ok {
+				return
+			}
+			log.Println("Pulse")
+		case r, ok := <-results:
+			if !ok {
+				return
+			}
+			log.Printf("result: %v\n", r.Second())
+		case <-time.After(timeout):
+			fmt.Println("timeout")
+			return
+		}
+	}
+}
+
+var doWork = func(
+	done <-chan interface{},
+	pulseInterval time.Duration,
+) (
+	<-chan interface{},
+	<-chan time.Time,
+) {
+	heartbeat := make(chan interface{})
+	results := make(chan time.Time)
+
+	go func() {
+		defer close(heartbeat)
+		defer close(results)
+
+		pulse := time.Tick(pulseInterval)
+		workGen := time.Tick(2 * pulseInterval)
+
+		sendPulse := func() {
+			select {
+			case heartbeat <- struct{}{}:
+			default:
+			}
+		}
+
+		sendResult := func(r time.Time) {
+			for {
+				select {
+				case <-done:
+					return
+				case <-pulse:
+					sendPulse()
+				case results <- r:
+					return
+				}
+			}
+		}
+
+		for {
+			select {
+			case <-done:
+				return
+			case <-pulse:
+				sendPulse()
+			case r := <-workGen:
+				sendResult(r)
+			}
+		}
+	}()
+
+	return heartbeat, results
+}
